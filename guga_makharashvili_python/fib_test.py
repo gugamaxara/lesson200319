9a72eb32a80ff1bfff2c752ceca0719c407e7ce2
gugamaxara <gugamaxara@github.com>
2020-03-19 17:39:24 +0400

    added new testcase2

--- guga_makharashvili_python/fib_test.py
+++ guga_makharashvili_python/fib_test.py
@@ -5,6 +5,11 @@ class FibTest(unittest.TestCase):
     
     def testcase1(self):
         self.assertEqual(1, fib_recursion(1))
+    
+    def testcase2(self):
+        self.assertEqual(2, fib_recursion(2))
+    
+    
 
 if __name__ == '__main__':
     unittest.main()
