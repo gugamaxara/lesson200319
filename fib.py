ba7d36f2486f04f18eb1abc8e5c13e0fd9973893
gugamaxara <gugamaxara@github.com>
2020-03-19 14:43:45 +0400

    minor change in conditions

--- fib.py
+++ fib.py
@@ -4,9 +4,11 @@
 def fib_recursion(n):
     if n < 0:
         return -1
-    elif n == 0:
-        return 0
     elif n == 1:
+        return 0
+    elif n == 2:
         return 1
     else:
         return fib_recursion(n-1) + fib_recursion(n-2)
+
+print(fib_recursion(9))
